2c76d5db900485f561469513519edc00d2021e66
Amanda <aleisero@ucsc.edu>
2018-08-02 21:07:34 -0700

    switched from p5js to phaser for easier asset handling

---
+++ source/main.js
@@ -0,0 +1,20 @@
+//10 Million Children's Books
+//a generative project by Amanda Leiserowitz & J. Alexander Carney
+// :3
+
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+
+function preload() {
+	// preload assets
+}
+
+function create() {
+	//set BG color
+	game.stage.backgroundColor = "#80ccff"
+	
+}
+
+function update() {
+	//update function
+}
