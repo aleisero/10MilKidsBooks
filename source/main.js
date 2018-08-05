1db0679435664ba620178ea5339454aa915ec5f8
Amanda <aleisero@ucsc.edu>
2018-08-05 16:26:51 -0700

    Tracery able to run grammer.

--- source/main.js
+++ source/main.js
@@ -7,6 +7,17 @@ var game = new Phaser.Game(1000, 600, Phaser.AUTO, '', { preload: preload, creat
 
 function preload() {
 	// preload assets
+	//var tracery = require('tracery-grammar');
+	
+	var grammar = tracery.createGrammar({
+	'animal': ['panda','fox','capybara','iguana'],
+	'emotion': ['sad','happy','angry','jealous'],
+	'origin':['I am #emotion.a# #animal#.'],
+	});
+
+	//grammar.addModifiers(tracery.baseEngModifiers); 
+
+	console.log(grammar.flatten('#origin#'));
 }
 
 function create() {
